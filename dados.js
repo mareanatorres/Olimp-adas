ed095148c3567d1248e378fa0a8e0ba8f298ea20
Mariana Torres <161315190+mareanatorres@users.noreply.github.com>
2024-09-06 11:46:39 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista profissional brasileira que se tornou a mais jovem medalhista olímpica da história do Brasil. Conhecida por sua habilidade e carisma, ela é um dos maiores nomes do skate mundial.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags:"skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nos Jogos Olímpicos de Paris 2024. Sua técnica apurada e determinação a tornaram uma das principais atletas do judô brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags:"judô judo judoca judoka"
+    },
+       
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é, sem dúvida, um dos maiores nomes da ginástica artística brasileira e mundial. Nascida em Guarulhos, a atleta conquistou o coração dos brasileiros com sua determinação e talento.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags:"ginástica ginastica ouro"
+    },
+];
